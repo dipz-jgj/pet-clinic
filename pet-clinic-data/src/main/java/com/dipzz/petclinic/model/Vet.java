c22d6463f5bfbc2ed5a6f90bfd04ee7473de9e2b
dipzfals87 <dipfals@gmail.com>
2021-01-08 14:07:04 +0700

    Update vet with JPA entity mappings. Closes #37

--- pet-clinic-data/src/main/java/com/dipzz/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/dipzz/petclinic/model/Vet.java
@@ -1,10 +1,18 @@
 package com.dipzz.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    // Eager means that JPA is going to try to load everything all at once
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vets_specialties",
+        joinColumns = @JoinColumn(name = "vet_id"),
+        inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialties = new HashSet<>();
 
     public Set<Speciality> getSpecialties() {

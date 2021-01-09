46034ff1e8edc6459eaa7c2b255f2660bf86f218
dipzfals87 <dipfals@gmail.com>
2021-01-09 14:35:03 +0700

    Added spring data JPA service for vet specialty. Closes #44

--- pet-clinic-data/src/main/java/com/dipzz/petclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/dipzz/petclinic/services/map/OwnerMapService.java
@@ -13,13 +13,13 @@ import java.util.Set;
 // Implement ke interface OwnerService utk function2nya
 
 @Service
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService{
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService{
 
     // Tambahkan class relasi owner ke pettype & pet
     private final PetTypeService petTypeService;
     private final PetService petService;
 
-    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+    public OwnerMapService(PetTypeService petTypeService, PetService petService) {
         this.petTypeService = petTypeService;
         this.petService = petService;
     }
